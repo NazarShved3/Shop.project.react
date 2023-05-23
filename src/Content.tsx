890b3cffe4b8ff0e44d9e233c8e253f481f13589
NazarShved3 <129195013+NazarShved3@users.noreply.github.com>
2023-05-23 20:10:13 +0300

    added content.tsx

---
+++ src/Content.tsx
@@ -0,0 +1,23 @@
+ type ContentType = {
+    text1: string
+     text2: string
+ }
+
+
+//  export const Content = ({text1, text2}) : 
+//      return ( 
+//          <React.Fragment>
+    
+//      <p>
+//          Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, magni error, voluptatum consectetur ducimus perspiciatis sequi esse sunt nisi numquam, ab dolores reiciendis possimus! Debitis quis veniam nam culpa sunt!
+//          </p>
+// <p>
+//          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis nam ducimus provident, blanditiis ipsam vel, dolor ut necessitatibus repellat temporibus, natus in accusantium corporis eius harum praesentium dignissimos inventore dicta?
+
+//      </p>
+    
+//      </React.Fragment>
+//     )
+//  }
+
+//  export default Content
--- src/index.tsx
+++ src/index.tsx
@@ -2,30 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import Title from './Title'
+// import Content from './Content'
 
 
-const Content = () => {
-    return ( 
-        <React.Fragment>
-    
-    <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, magni error, voluptatum consectetur ducimus perspiciatis sequi esse sunt nisi numquam, ab dolores reiciendis possimus! Debitis quis veniam nam culpa sunt!
-        </p>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis nam ducimus provident, blanditiis ipsam vel, dolor ut necessitatibus repellat temporibus, natus in accusantium corporis eius harum praesentium dignissimos inventore dicta?
 
-    </p>
-    
-    </React.Fragment>
-    )
-}
 
 const App = () => {
     return(
         <>
             <Title title="App" />
             <Title title="React" num={1}/>
-            <Content/> 
+            {/* <Content/>  */}
         </>
     )
 }
