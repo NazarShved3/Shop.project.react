117a6820b8b6838c13ddd5d35dbb909c51af6e59
NazarShved3 <129195013+NazarShved3@users.noreply.github.com>
2023-05-23 20:40:11 +0300

    added imports

---
+++ src/Container/App/App.tsx
@@ -0,0 +1,10 @@
+import Header from 'Container/Header/Header'
+const App = () => {
+    return (
+    <>
+    < Header/>
+    </>
+    )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from 'Container/App/App'
 
-
-
-
-const App = () => {
-    return(
-        <></>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
