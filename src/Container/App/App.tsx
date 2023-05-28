68c330327739f059645a22e98f5d2aa5a7378d26
NazarShved3 <129195013+NazarShved3@users.noreply.github.com>
2023-05-28 23:07:47 +0300

    imported StyledEngineProvider

--- src/Container/App/App.tsx
+++ src/Container/App/App.tsx
@@ -1,12 +1,13 @@
 import { CssBaseline } from '@mui/material'
 import Header from 'Container/Header/Header'
+import { StyledEngineProvider } from '@mui/material/styles'
 
 const App = () => {
     return (
-    <>
+    <StyledEngineProvider injectFirst>
         <CssBaseline/>
         < Header/>
-    </>
+    </StyledEngineProvider>
     )
 }
 
